a397f5d7fc6ea63a0cf9922d93fab938c352f061
qxq <812573459@qq.com>
2020-12-27 13:08:55 +0800

    111

--- model/user.js
+++ model/user.js
@@ -9,13 +9,13 @@ module.exports =  class User extends require('./model'){
      * 用户数据
      */
 
-    static login(username,password){
-        return new Promise((resolve,reject)=>{
-            let sql = 'SELECT id,username FROM user WHERE username = ? AND password = ?'
-            this.query(sql,[username,password]).then(results=>{
+    static login(username, password) {
+        return new Promise((resolve, reject) => {
+            let sql = 'SELECT id,username FROM `user` WHERE username = ? AND PASSWORD = ?'
+            this.query(sql, [username, password]).then(results => {
                 resolve(results[0])
-            }).catch(err=>{
-                console.log(`登录失败：${err.message}`)
+            }).catch(err => {
+                console.log('登录失败：' + err.message)
                 reject(err)
             })
         })
